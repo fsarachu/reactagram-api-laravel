9127af671a29f614f50c36f25fc6f326c0f6faa0
fsarachu <fsarachu@gmail.com>
2017-06-22 18:46:20 -0300

    Setup laravel-json-api Service Provider and Facade alias

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third Party Providers...
+         */
+        CloudCreativity\LaravelJsonApi\ServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +213,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'JsonApi' => CloudCreativity\LaravelJsonApi\Facade::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
